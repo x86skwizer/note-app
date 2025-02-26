375d8f20962040ed227f93e124d48c1bddf765ad
x86skwizer <yassineamrire@protonmail.com>
2025-02-26 08:27:44 +0000

    Init Login & Register Front Pages

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,5 +1,8 @@
 function NotFound() {
-	return <div>Not Found</div>
+	return <div>
+		<h1>404 Not Found</h1>
+		<p>The page you're looking for does not exist!</p>
+	</div>
 }
 
 export default NotFound
